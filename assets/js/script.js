5f9c02807e800d1c1794fd1918cdfff08bb1d02c
Trello backend <trellobackendnode@gmail.com>
2026-03-17 03:07:40 +0100

    updated some pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,43 @@
-
-
-$(document).ready(function(){
-    $('.feedback-slider').owlCarousel({
-        loop: false,
-        margin: 10,
-        nav: true,
-        items: 1,
-        autoplay: true,
-        navText: ["<i class = 'fas fa-arrow-left'></i>", "<i class = 'fas fa-arrow-right'></i>"]
+$(document).ready(function () {
+  // Initialize carousel only if element exists
+  if ($(".feedback-slider").length) {
+    $(".feedback-slider").owlCarousel({
+      loop: false,
+      margin: 10,
+      nav: true,
+      items: 1,
+      autoplay: true,
+      navText: [
+        "<i class = 'fas fa-arrow-left'></i>",
+        "<i class = 'fas fa-arrow-right'></i>",
+      ],
     });
+  }
 
-    // stop animation on resize
-    let resizeTimer;
-    $(window).resize(function(){
-        $(document.body).addClass('resize-animation-stopper');
-        clearTimeout(resizeTimer);
-        resizeTimer = setTimeout(() => {
-            $(document.body).removeClass('resize-animation-stopper');
-        }, 400);
-    });
+  // stop animation on resize
+  let resizeTimer;
+  $(window).resize(function () {
+    $(document.body).addClass("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      $(document.body).removeClass("resize-animation-stopper");
+    }, 400);
+  });
 
-    $('.navbar-show-btn').click(function(){
-        $('.navbar-box').addClass('navbar-box-show');
-    });
+  // Mobile menu functionality
+  $(".navbar-show-btn").click(function () {
+    $(".navbar-box").addClass("navbar-box-show");
+  });
 
-    $('.navbar-hide-btn').click(function(){
-        $('.navbar-box').removeClass("navbar-box-show");
-    })
+  $(".navbar-hide-btn").click(function () {
+    $(".navbar-box").removeClass("navbar-box-show");
+  });
+
+  // Set active nav based on current page
+  var currentPage = window.location.pathname.split("/").pop();
+  if (currentPage === "" || currentPage === "index.html") {
+    $('.nav-link[href="index.html"]').addClass("nav-active");
+  } else {
+    $('.nav-link[href="' + currentPage + '"]').addClass("nav-active");
+  }
 });
